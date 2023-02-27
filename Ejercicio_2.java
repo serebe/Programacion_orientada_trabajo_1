a24a1d192c6b21a8965724e77766c3d19a4ff2ca
Sebastian Restrepo <64997621+serebe@users.noreply.github.com>
2023-02-27 00:37:55 -0500

    Create Ejercicio_2.java

---
+++ Ejercicio_2.java
@@ -0,0 +1,15 @@
+
+//ejercicio resuelto 5
+
+public class Ejercicio_2 {
+    public static void main(String[] args) {
+       double suma,x,y;
+       suma=0;
+       x=20;
+       y=40;
+       suma=suma+x;
+       x=Math.pow(y, 2)+x;
+       suma=suma+ (x/y);
+       System.out.println("EL VALOR DE LA SUMA ES "+suma);       
+    }
+}
